bced38299281958c6abb9919af1e3f7fc6af29dd
Karla Jara <jara.karla.43@gmail.com>
2021-05-13 16:49:04 -0500

    maven exercises

---
+++ src/main/java/Input.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+import static org.apache.commons.lang3.StringUtils.isNumeric;
+
+public class Input {
+    public static void main(String[] args){
+
+        System.out.println("Please, enter something of your choice: ");
+
+    }
+
+    public static String infoEntered(){
+        Scanner userInput = new Scanner(System.in);
+        String entered = userInput.next();
+        for (St)
+        if (isNumeric(entered)){
+
+        }
+    }
+}
